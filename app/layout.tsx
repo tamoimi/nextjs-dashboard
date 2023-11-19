6f337cce7999ec3dce6b6a8ee24a48c3a913abcd
tamoimi <tamiortami@gmail.com>
2023-11-19 15:19:55 +0900

    style: added new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
-import "@/app/ui/global.css"
+import "@/app/ui/global.css";
+import { inter } from "./ui/fonts";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
-
